be520b2abc4fadf567e810a4c0e9cbd779515fe9
Pham Van Hung <137643495+PhamVanHung412004@users.noreply.github.com>
2025-04-19 18:42:09 +0700

    Update app.py
    fix app.py

--- app.py
+++ app.py
@@ -10,6 +10,5 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db.init_app(app)
 
 app.register_blueprint(routes)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
